27fe101c13ab63aab3ad569dcd1a5c27f34c86ab
igrackoz <hectoraop@outlook.com>
2025-04-18 20:14:21 -0500

    asda

---
+++ src/app/pages/about/about-page.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './about-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class AboutPageComponent { }
---
+++ src/app/pages/contact/contact-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'contact-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class ContactPageComponent { }
